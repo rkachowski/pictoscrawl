5c43d3288cdbd477977a17c4a53591ca7fe94998
Donald Hutchison <dhutch@firebastard>
2012-04-13 14:29:26 +0100

    using redis to save user login data aww yeah

--- src/twisted/plugins/users.py
+++ src/twisted/plugins/users.py
@@ -1,4 +1,6 @@
 from huru import CommandPlugin, Response
+import redis
+import datetime
 
 class RoomUsersPlugin(CommandPlugin):
     """List users within the room"""
@@ -6,6 +8,13 @@ class RoomUsersPlugin(CommandPlugin):
         users = cnx.room_users()
         #write users to new user
         cnx.write(Response("users",*users))
+
+        r_server = redis.StrictRedis(host="localhost",password="firebastard-redis")
+
+        user_no = r_server.incr("pictoscrawl:total_users")
+        key = "pictoscrawl:users:"+str(user_no)
+        values = dict(name=cnx.userid, date=str(datetime.datetime.now()))
+        r_server.hmset(key, values)
 
 #######
 USERS = RoomUsersPlugin()
